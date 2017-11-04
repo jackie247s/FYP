ccf2011388b167c55b389f627fa2bf5c07f50626
jackie247s <jackie247s@gmail.com>
2017-11-04 16:17:50 +0500

    Added new reusable component screen.js to center layout in app, Cleaned code according to eslint taake wo dimaag khana band kare

--- src/components/inputbox.js
+++ src/components/inputbox.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import {Text} from 'react-native';
-import { Container, Header, Content, Item, Input } from 'native-base';
+import React from 'react';
+import { Item, Input } from 'native-base';
 
-const InputBox = (props) =>{
+const InputBox = (props) => {
 	return (
-      <Item style={{marginTop:20}} rounded>
-            <Input  placeholder={props.placeholderText}/>
+      <Item style={{ marginTop: 20 }} rounded>
+            <Input placeholder={props.placeholderText} />
       </Item>
     );
-}
+};
 
 export default InputBox;
