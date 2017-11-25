e1abfe40ded4ba3b36ae398613d197e30d78dc1a
jackie247s <jackie247s@gmail.com>
2017-11-25 13:05:44 +0500

    Cleaned code in renewpromotion.js

--- src/components/renewpromotion.js
+++ src/components/renewpromotion.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, List, ListItem, Thumbnail, Body, Button, Right, Text } from 'native-base';
+import { Container, List } from 'native-base';
 import Promotion from './RenewPromotion/promotion';
 
 const RenewPromotion = () => {
