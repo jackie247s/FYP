6dd310bbc226cc075f12df333177b1a511aa4592
jackie247s <jackie247s@gmail.com>
2017-11-25 12:58:56 +0500

    Cleaned code in index.js,popup.js

--- src/Main/popup.js
+++ src/Main/popup.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {StyleSheet,Text,View,Image} from 'react-native';
-import {Actions} from 'react-native-router-flux';
-import {Button} from 'native-base';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { Button } from 'native-base';
 
-class Popup extends React.Component{
-
+class Popup extends React.Component {
   componentDidMount() {
-    setTimeout(wait,3000);
-      function wait(){
-      Actions.TabBar();
+    setTimeout(wait, 3000);
+      function wait() {
+        Actions.TabBar();
+      }
   }
-  }
-	render(){
-		return(
-        <View style={{justifyContent: 'center',alignItems: 'center',marginTop:200,flexDirection:'row'}}>
+
+	render() {
+		return (
+        <View
+          style={styles.popUpStyle}
+        >
             <Image style={styles.image} source={require('../images/logo.png')}/>
         </View>
 			);
@@ -21,14 +23,18 @@ class Popup extends React.Component{
 }
 
 const styles = StyleSheet.create({
-  
-  image: {
-    height:250,
-    width:250,
-    marginLeft:0,
-    resizeMode: 'contain'
+  popUpStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 200,
+    flexDirection: 'row'
   },
-  
+  image: {
+    height: 250,
+    width: 250,
+    marginLeft: 0,
+    resizeMode: 'contain'
+  }
 });
 
 export default Popup;
