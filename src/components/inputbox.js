0b0407b83f2c3fa298f55674547e726940f29df7
jackie247s <jackie247s@gmail.com>
2017-11-25 18:48:10 +0500

    Refactored it to accept value and callback function from parent

--- src/components/inputbox.js
+++ src/components/inputbox.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Item, Input, Icon } from 'native-base';
 
 const InputBox = (props) => {
+	const { placeholderText, value, onChangeText } = props;
+
 	return (
 		//#d9534f
       <Item style={{ marginLeft: 8, marginRight: 8, marginTop: 10 }} rounded>
 				<Icon style={{ color: '#fff' }} active name={props.Icon} />
 				<Input
-					placeholder={props.placeholderText}
+					placeholder={placeholderText}
 					placeholderTextColor={'#dbd8d8'}
-					value={props.value}
-					onChangeText={props.onChangeText}
+					value={value}
+					onChangeText={onChangeText}
 				/>
       </Item>
     );
