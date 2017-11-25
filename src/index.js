333f681f38e7276fa72a5486605b741411f6ac60
jackie247s <jackie247s@gmail.com>
2017-11-25 12:59:27 +0500

    Cleaned code in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-
-import {Scene, Router} from 'react-native-router-flux';
+import { Scene, Router } from 'react-native-router-flux';
 
 import Signup from './components/signup';
 import Login from './components/login';
@@ -8,30 +7,24 @@ import NewPromotion from './components/newpromotion';
 import RenewPromotion from './components/renewpromotion';
 import MainScreen from './Main';
 import Popup from './Main/popup';
-import TabBar from './components/tabBar'
-class Routes extends React.Component{
+import TabBar from './components/tabBar';
 
-	render(){
-		return(
-      
+class Routes extends Component {
+	render() {
+		return (
 			<Router>
-        <Scene hideNavBar={true} key="root">
-         <Scene key="Main" component={MainScreen}/>
-           <Scene key="Popup" component={Popup} title="Password"/>
-        <Scene key="TabBar" component={TabBar} title="TabBar" hideNavBar={true}/>
-        <Scene key="Login" component={Login} title="login" hideNavBar={true}/>
-        <Scene key="Signup" component={Signup} title="Register"/>
-        <Scene key="NewPromotion" component={NewPromotion} title="NewPromotion"/>
-        <Scene key="RenewPromotion" component={RenewPromotion} title="RenewPromotion"/>
-          
+        <Scene hideNavBar key="root">
+					<Scene key="Main" component={MainScreen} />
+					<Scene key="Popup" component={Popup} title="Password" />
+					<Scene key="TabBar" component={TabBar} title="TabBar" hideNavBar />
+					<Scene key="Login" component={Login} title="login" hideNavBar />
+					<Scene key="Signup" component={Signup} title="Register" />
+					<Scene key="NewPromotion" component={NewPromotion} title="NewPromotion" />
+					<Scene key="RenewPromotion" component={RenewPromotion} title="RenewPromotion" />
         </Scene>
       </Router>
     );
-
 	}
 }
-
-
-
 
 export default Routes;
