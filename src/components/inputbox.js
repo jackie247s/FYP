d1484b88260c863757057ed7e1afc176e8339b2d
jackie247s <jackie247s@gmail.com>
2017-11-25 14:22:43 +0500

    Refactored inputbox to take value from parent and to accept onChangeText callback function

--- src/components/inputbox.js
+++ src/components/inputbox.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Item, Input,Icon } from 'native-base';
+import { Item, Input, Icon } from 'native-base';
 
 const InputBox = (props) => {
 	return (
 		//#d9534f
-      <Item style={{ marginLeft:8,marginRight:8,marginTop:10}} rounded>
-       <Icon style={{color:'#fff'}} active name={props.Icon} />
-            <Input placeholder={props.placeholderText} placeholderTextColor={'#dbd8d8'} />
+      <Item style={{ marginLeft: 8, marginRight: 8, marginTop: 10 }} rounded>
+				<Icon style={{ color: '#fff' }} active name={props.Icon} />
+				<Input
+					placeholder={props.placeholderText}
+					placeholderTextColor={'#dbd8d8'}
+					value={props.value}
+					onChangeText={props.onChangeText}
+				/>
       </Item>
     );
 };
