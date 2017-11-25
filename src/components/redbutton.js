3b601cb9583a1a54588ffe477196063aacb21c98
jackie247s <jackie247s@gmail.com>
2017-11-25 14:24:11 +0500

    Cleaned code and added onPress callback function handler

--- src/components/redbutton.js
+++ src/components/redbutton.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Text } from 'native-base';
 import { Dimensions } from 'react-native';
 
 const width = Dimensions.get('window').width;
-const mar = width-50;
-const RedButton = (props) =>{
-	return(
-		 <Button style={styles.buttonStyle} rounded light>
-			 <Text style={{fontSize:18,fontWeight:'bold'}}>{props.buttonText}</Text>
-		 </Button>
-		 );
+const mar = width - 50;
+
+const RedButton = (props) => {
+	return (
+			<Button style={styles.buttonStyle} rounded light onPress={props.onPress}>
+				<Text style={{ fontSize: 18, fontWeight: 'bold' }}>{props.buttonText}</Text>
+			</Button>
+		);
 };
 
 const styles = {
   buttonStyle: {
-  	flexDirection:'row',
-    width:mar,
+		flexDirection: 'row',
+    width: mar,
     justifyContent: 'center',
     alignItems: 'center',
     height: 50,
-    marginTop:20,
+    marginTop: 20,
     padding: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 5 },
     shadowOpacity: 0.2,
     elevation: 5,
     position: 'relative',
-    
+
   }
 
 };
