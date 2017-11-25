82e9a10ffbfdde93fb7491d59d910bd50abf5c54
jackie247s <jackie247s@gmail.com>
2017-11-25 18:46:12 +0500

    Added firebase, retrieving data from database and displaying it

--- src/components/renewpromotion.js
+++ src/components/renewpromotion.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import { Container, List } from 'native-base';
+import { Container, List, Spinner } from 'native-base';
 import firebase from './firebase';
 import Promotion from './RenewPromotion/promotion';
 
 class RenewPromotion extends Component {
+  constructor() {
+    super();
+    this.state = {
+      promotions: [],
+      loading: true
+    };
+  }
+
   componentWillMount() {
-    
+    const promotionsRef = firebase.database().ref('promotions');
+    promotionsRef.once('value')
+      .then(this.getPromotions.bind(this));
+  }
+
+  getPromotions(snapshot) {
+    const promotionsArr = [];
+    let obj = {};
+    snapshot.forEach(childsnapshot => {
+      obj = {
+        productname: childsnapshot.val().productname,
+        productdesc: childsnapshot.val().productdesc
+      };
+      promotionsArr.push(obj);
+    });
+    console.log(promotionsArr);
+    this.setState({ promotions: promotionsArr, loading: false });
+  }
+
+  renderPromotions() {
+    if (this.state.loading) {
+      return <Spinner />;
+    }
+    return this.state.promotions.map(promotion =>
+      <Promotion promotion={promotion} />);
   }
 
   render() {
     return (
       <Container>
         <List>
-          <Promotion />
+          {this.renderPromotions()}
         </List>
       </Container>
     );
