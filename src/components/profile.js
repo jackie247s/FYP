e84f6f8c5e79ce2ab74a0c833f5b4ac5e1363353
jackie247s <jackie247s@gmail.com>
2017-12-08 23:03:07 +0500

    Made profile state dynamic by retrieving data from firebase database

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Thumbnail, Button } from 'native-base';
+import { Thumbnail, Button, Spinner } from 'native-base';
+import firebase from '../firebase';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userid: this.props.userid,
+      name: '',
+      email: '',
+      loading: true
+    };
+  }
+
+  componentWillMount() {
+    const userInfoRef = firebase.database().ref('merchants/' + this.state.userid);
+    userInfoRef.on('value', snapshot => {
+      console.log(snapshot.val());
+      const children = snapshot.val();
+      const userInfo = children[Object.keys(children)[0]];
+      console.log(userInfo);
+      const name = userInfo.Name;
+      const email = userInfo.Email;
+      const loading = false;
+      this.setState({ name, email, loading });
+    });
+  }
+
   onButtonPress() {
 
   }
 
-  render() {
+  renderProfile() {
     const { containerStyle, textStyle, buttonContainerStyle, buttonStyle } = styles;
+
+    if (this.state.loading) {
+      return <Spinner />;
+    }
 
     return (
       <View style={containerStyle}>
         <Thumbnail large source={require('../images/placeholder_profile_photo.png')} />
-        <Text style={textStyle}>Ali Usman</Text>
-        <Text style={textStyle}>k142170@nu.edu.pk</Text>
+        <Text style={textStyle}>{this.state.name}</Text>
+        <Text style={textStyle}>{this.state.email}</Text>
         <View style={buttonContainerStyle}>
           <Button style={buttonStyle} rounded light onPress={this.onButtonPress.bind(this)}>
             <Text>Change Picture</Text>
           </Button>
         </View>
+      </View>
+    );
+  }
+
+  render() {
+    const { containerStyle } = styles;
+
+    return (
+      <View style={containerStyle}>
+        {this.renderProfile()}
       </View>
     );
   }
