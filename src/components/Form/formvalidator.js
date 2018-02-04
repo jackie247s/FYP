233ec139995a35761c80e512a439efa15c275bb4
jackie247s <jackie247s@gmail.com>
2018-02-04 16:37:25 +0500

    Added new methods for checking mobile and email validity

--- src/components/Form/formvalidator.js
+++ src/components/Form/formvalidator.js
@@ -2,19 +2,33 @@
 // Does not use react or JSX
 
 const FormValidator = {
-   checkIfFieldEmpty() {
-    const entries = Object.entries(this.state);
+   checkIfFieldEmpty(formObject) {
+    const entries = Object.entries(formObject);
     let value;
 
     for (let i = 0; i < entries.length; i++) {
       value = entries[i][1];
       if (value === '') {
-        alert('Please fill out all the fields');
         return true;
       }
     }
 
     return false;
+  },
+
+  checkValidMobile(mobileNo) {
+    let mobileIsValid = true;
+
+    if (mobileNo.length !== 11) {
+      mobileIsValid = false;
+    }
+
+    return mobileIsValid;
+  },
+
+  checkValidEmail(email) {
+    const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return reg.test(email);
   }
 };
 
