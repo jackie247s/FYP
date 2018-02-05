70cd8552a0c94c73e48c80af308ea3a91522ec56
jackie247s <jackie247s@gmail.com>
2018-02-05 19:38:17 +0500

    Styled for better whitespacing

--- src/components/termsconditions.js
+++ src/components/termsconditions.js
@@ -9,11 +9,21 @@ const TermsConditions = (props) => {
     <Image source={require('../images/bg.png')} style={backgroundImageStyle}>
       <View style={containerStyle}>
         <Text style={textStyle}>You must agree to the following terms and conditions:</Text>
-        <Text style={textStyle}>1. The bank will keep 100% of the revenue made from the transaction</Text>
-        <Text style={textStyle}>2. The bank will charge you Rs.50,000 per month on a subscription model for this service</Text>
-        <Text style={textStyle}>3. The bank's authority is final. If it wishes, it can terminate your subscription without warning</Text>
+        <Text style={textStyle}>
+          1. The bank will keep 100% of the revenue made from the transaction
+        </Text>
+        <Text style={textStyle}>
+          2. The bank will charge you Rs.50,000 per month on a subscription model for this service
+        </Text>
+        <Text style={textStyle}>
+          3. The bank's authority is final.
+          If it wishes, it can terminate your subscription without warning.
+        </Text>
         <View style={buttonContainerStyle}>
-          <FormButton1 buttonText="I Accept" onPress={() => { Actions.AcquireMerchant({ user: props.user }); }} />
+          <FormButton1
+            buttonText="I Accept"
+            onPress={() => { Actions.AcquireMerchant({ user: props.user }); }}
+          />
         </View>
       </View>
     </Image>
@@ -29,7 +39,9 @@ const styles = {
   containerStyle: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: 10
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20
   },
   textStyle: {
     color: 'white',
